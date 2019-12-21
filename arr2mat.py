2a1932240a94c48b7282c598baf6c609d4abf2b2
hafniumhu <hafniumhu@gmail.com>
2019-12-20 20:20:03 -0500

    arr2mat.py: initialize matrix

--- arr2mat.py
+++ arr2mat.py
@@ -1,5 +1,36 @@
+"""
+arr2mat
+
+"""
 import math
 
-def arr2matrix(arr):
-    rates = [[math.inf for x in range(70)] for x in range(70)]
-    index = [[i for i in range(7)] for i in range(10)]
+
+"""
+Input: 7 arrays containing exchange rates based on USD
+Output: a matrix of exchange rates
+"""
+def arr2mat(arr):
+    # Initialize rate to infinity
+    day = len(arr)
+    curr = len(arr[0])
+    size = day * curr
+    rates = [[math.inf for x in range(size)] for x in range(size)]
+
+    # Construct rates
+    for i in range(size):
+        # Fill out by row
+        curr_remainder = i % day
+        curr_start = i - curr_remainder
+
+        # Get backward entries
+        if curr_remainder == day - 1:
+            rates[i][curr_start] = 1
+
+        for j in range(size):
+            # Same currency entries (1's)
+            if i <= j < curr_start + day: # days = 3
+                rates[i][j] = 1
+
+            # Different currency entries
+            if j >= curr_start + day:
+                
